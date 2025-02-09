276cfa413e3d9dc0fadaf35897234821d306a3b8
Orest <opokotylenkowork@gmail.com>
2025-02-09 12:28:04 +0100

    initial commit

---
+++ calculator-git-assignment/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace calculator_git_assignment
+{
+    internal class Calculator
+    {
+        public void Add(double a, double b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public void Subtract(double a, double b)
+        {
+            Console.WriteLine(a - b);
+        }
+
+        public void Multiply(double a, double b)
+        {
+            Console.WriteLine(a * b);
+        }
+
+        public void Divide(double a, double b)
+        {
+            Console.WriteLine(a / b);
+        }
+    }
+}
---
+++ calculator-git-assignment/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.RegularExpressions;
+
+namespace calculator_git_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new();
+            program.Start();
+        }
+
+        private void Start()
+        {
+            string input = string.Empty;
+
+            do
+            {
+                Console.WriteLine("What do you want to calculate?");
+                input = Console.ReadLine().Replace(" ", "");
+                Match match = Regex.Match(input, @"(\d+)([+\-*/])(\d+)");
+
+                if (match.Success)
+                {
+                    int a = int.Parse(match.Groups[1].Value);
+                    string operation = match.Groups[2].Value;
+                    int b = int.Parse(match.Groups[3].Value);
+
+                    Calculate(operation, a, b);
+                }
+
+            } while (input != "stop");
+        }
+
+        private void Calculate(string operation, double a, double b)
+        {
+            Calculator calculator = new();
+            switch (operation)
+            {
+                case "+":
+                    calculator.Add(a, b);
+                    break;
+                case "-":
+                    calculator.Subtract(a, b);
+                    break;
+                case "*":
+                    calculator.Multiply(a, b);
+                    break;
+                case "/":
+                    calculator.Divide(a, b);
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation");
+                    break;
+            }
+        }
+    }
+}
